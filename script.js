3591e93dcdb25278c22d4aa792b90e02cda19743
nsaffa682 <nsaffa682@west-mec.org>
2026-05-14 18:19:52 -0700

    s

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-window.addEventListener("scroll", () => {
 
-    const nav = document.querySelector("nav");
-
-    if(window.scrollY > 50){
-        nav.classList.add("nav-scrolled");
-    } else {
-        nav.classList.remove("nav-scrolled");
-    }
-
-});
-
-
-
-const revealElements = document.querySelectorAll(
-    ".card, .industry-card, .career-box, .path-card, .timeline-item, .source-card"
-);
 
 function revealOnScroll(){
 
-    const triggerBottom = window.innerHeight * 0.85;
-
-    revealElements.forEach((element) => {
-
-        const boxTop = element.getBoundingClientRect().top;
-
-        if(boxTop < triggerBottom){
-            element.classList.add("show");
-        }
-
-    });
+    element.classList.add("show");
 
 }
 
